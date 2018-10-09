e3bdad43045fe2abbdbc873409d586fbfa940862
ndking09 <42976254+ndking09@users.noreply.github.com>
2018-10-09 19:39:26 -0400

    Add files via upload

---
+++ quiz3/Quiz3/src/pkgCore/Deck.java
@@ -0,0 +1,69 @@
+package pkgCore;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+import pkgEnum.eRank;
+import pkgEnum.eSuit;
+import pkgException.DeckException;
+
+public class Deck {
+
+	private ArrayList<Card> cardsInDeck = new ArrayList<Card>();
+
+	public Deck() {
+		for (eSuit eSuit : eSuit.values()) {
+			for (eRank eRank : eRank.values()) {
+				cardsInDeck.add(new Card(eSuit, eRank));
+			}
+		}
+		Collections.shuffle(cardsInDeck);
+	}
+
+	public Card Draw() throws DeckException {
+
+		if (cardsInDeck.size() == 0)
+		{
+			throw new DeckException(this);
+		}
+		return cardsInDeck.remove(0);
+
+	}
+
+	public Card Draw(eSuit eSuit) {
+		for (Card c : cardsInDeck) {
+			if (c.geteSuit() == eSuit) {
+				cardsInDeck.remove(c);
+				return (c);
+			}
+		}
+		return (null);
+	}
+
+	public int getiDeckCount()
+	{
+		return cardsInDeck.size();
+	}
+	
+	public int getRemaining(Object eNum) {
+		int counter = 0;  
+		if (eNum instanceof eSuit) {
+			for (Card c: cardsInDeck) {
+				if (eNum == c.geteSuit()) {
+					counter ++;
+				}
+			}
+			return counter;
+		}
+		
+		if (eNum instanceof eRank) {
+			for (Card c: cardsInDeck) {
+				if (eNum == c.geteRank()) {
+					counter ++;
+				}
+			}
+			return counter;
+		}
+		return -1;
+	}
+}
